30d56005cbf9c4d9608cc6fe4e9f31ac0a930fdc
buket7 <67732340+buket7@users.noreply.github.com>
2021-05-08 07:14:07 +0300

    Add files via upload

---
+++ gps_mavros.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import rospy
+from sensor_msgs.msg import NavSatFix
+
+
+
+def gps_durum(data):
+    rospy.loginfo("Current Latitude degrees : %2f ", data.latitude)
+    rospy.loginfo("Current Longitude degrees : %2f ", data.longitude)
+    rospy.loginfo("Current Altitude : %2f ", data.altitude)
+    print("hi")
+
+def do_cur_gps():
+    rospy.init_node("mavcmd", anonymous=True)
+    rospy.Subscriber("/mavros/global_position/global",NavSatFix,gps_durum)
+    rospy.spin()
+
+
+do_cur_gps()    
